ebf685793eae9a34ecb5f6951c597732ec0d38dd
hyoni <jih70004@gmail.com>
2024-09-30 21:50:42 +0900

    home page 마크업, 스타일 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return
+
+  return (
+    <main className="home bg-default">
+      <header>
+        <h1>JANG JIHYUN</h1>
+        <h2>Portfolio</h2>
+        <p>Front-end Developer</p>
+      </header>
+      <ul className="local-nav">
+        <li><Link to="/about" >About</Link></li>
+        <li><Link to="/projects" >Projects</Link></li>
+      </ul>
+    </main>
+  )
 }
 export default Home;
