c182147585b832a699996224cc806fa60928a15c
hyoni <jih70004@gmail.com>
2024-10-04 00:55:40 +0900

    desktop project style update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,30 +10,33 @@ function Projects() {
   }
 
   return (
-    <main className="Projects bg-black">
-      <Animation delayBetween={300} initialDelay={200} duration={1000}>
-        <header>
+    <main className="project bg-black">
+      <header>
+        <Animation delayBetween={200} initialDelay={200} duration={1000}>
           <h1>PROJECT</h1>
-        </header>
-        <p>
-          퍼블리셔로 일하며 UI/UX에 대한 이해를 쌓을 수 있었습니다.
-          이 경험을 바탕으로 프론트엔드 개발자로서 성장하기 위해
-          지속적으로 학습하고 있습니다.
-        </p>
-        <section>
-          <ul className="Projects-nav">
+          <p>
+            퍼블리셔로 일하며 UI/UX에 대한 이해를 쌓을 수 있었습니다.<br className="br-pc" />
+            이 경험을 바탕으로 프론트엔드 개발자로서 성장하기 위해
+            지속적으로 학습하고 있습니다.
+          </p>
+        </Animation>
+      </header>
+      <section>
+        <ul className="project-nav">
+          <Animation delayBetween={300} initialDelay={600} duration={1000}>
             {projectList.map((project) => (
               <li
                 key={project.id}
                 onClick={() => handleDetail(project.id)}
-                tabIndex={0} // 키보드 탐색을 위해 추가
+                tabIndex={0}
               >
                 {project.title}
               </li>
             ))}
-          </ul>
-        </section>
-      </Animation>
+          </Animation>
+        </ul>
+      </section>
+
     </main>
   )
 }
